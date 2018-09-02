0870bf260ebf16a06f3d22b7e14c5f9a67ebaac4
haza290 <harry.g.schneider@gmail.com>
2018-09-02 21:41:46 +0100

    Working version but needs print method finished and needs optimisations

--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,35 @@ import java.util.*;
 public class Main
 {
 	private int target = 60;
-	private int maxNumOfDarts = 3;
+	private int maxNumOfDarts = 6;
 	private ArrayList<ArrayList<Integer>> solutions;
+	private ArrayList<ArrayList<Integer>> exploredSolutions;
 	
-	private HashMap<Integer,String[]> dartsHashmap;
-	private HashMap<Integer,String[]> endingDartsHashmap;
-	public static void main(String[] args)
-	{
+	private int[] allDartScores = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,
+		19,20,21,22,24,25,26,27,28,30,32,33,34,36,38,39,40,42,44,45,46,48,50,51,
+		52,54,56,57,60};
+	private int[] endingDartScores = {2,3,4,6,8,9,10,12,14,15,16,18,20,21,22,24,
+		25,26,27,28,30,32,33,34,36,38,39,40,42,44,45,46,48,50,51,52,54,56,57,60};
+	
+	public static void main(String[] args) {
 		new Main();
 	}
 	
 	public Main() {
-		createHashMaps();
 		this.solutions = new ArrayList<>();
+		this.exploredSolutions = new ArrayList<>();
+		System.out.println("Starting loopback");
 		loopback(new ArrayList<Integer>());
 		printSolutions();
+		System.out.println("done");
 	}
 	
-	private void createHashMaps() {
-		this.dartsHashmap = new HashMap<>();
-		this.dartsHashmap.put(20,new String[]{"20","D10"});
-		
-		this.endingDartsHashmap = new HashMap<>();
-	}
 	
-	private void loopback(ArrayList<Integer> currentDarts) {
-		if (0 == currentDarts.size()) {
-			for (Map.Entry<Integer,String[]> entry : this.endingDartsHashmap.entrySet()){
-				ArrayList newDarts = new ArrayList(currentDarts);
-				newDarts.add((Integer)entry.getKey());
-				loopback(newDarts);
-			}
-		}
-		if (isAlreadyExplored(currentDarts)) {
-			return;
-		}
-		 
+	private void loopback(final ArrayList<Integer> currentDarts) {
+		//if (isAlreadyExplored(currentDarts)) {
+		//	return;
+		//}
+		//this.exploredSolutions.add(currentDarts);
 		int sumOfCurrentDarts = sumDarts(currentDarts);
 		if (target == sumOfCurrentDarts) {
 			solutions.add(currentDarts);
@@ -50,24 +43,53 @@ public class Main
 		if (currentDarts.size() >= this.maxNumOfDarts) {
 			return;
 		}
-		for (Map.Entry<Integer,String[]> entry : dartsHashmap.entrySet()){
-			ArrayList newDarts = new ArrayList(currentDarts);
-			newDarts.add((Integer)entry.getKey());
-			loopback(newDarts);
+		if (0 == currentDarts.size()){
+			for (int endingDartScore : this.endingDartScores){
+				creatNewArrayAndLoopBack(currentDarts, endingDartScore);
+				return;
+			}
+		}
+		for (int dartScore : this.allDartScores){
+			creatNewArrayAndLoopBack(currentDarts, dartScore);
 		}
 	}
 	
-	private void printSolutions()
-	{
-		// TODO write this
+	private void creatNewArrayAndLoopBack(ArrayList currentDarts, int dart) {
+		ArrayList<Integer> newDartsArray = new ArrayList<>(currentDarts);
+		newDartsArray.add(dart);
+		loopback(newDartsArray);
+	}
+	
+	private void printSolutions() {
+		System.out.println("Solutions:");
+		for(ArrayList arrayList : this.solutions){
+			String solution = "";
+			for(Integer dart : arrayList){
+				solution += Integer.toString(dart) + ", ";
+			}
+			System.out.println(solution);
+		}
 	}
 	
 	private boolean isAlreadyExplored(ArrayList<Integer> currentDarts) {
-		// TODO write this
+		// TODO rewrite this to optimise it
+		Random rand = new Random();
+		if(rand.nextInt(100) == 99) {
+			System.out.println(currentDarts);
+		}
+		ArrayList<Integer> tempCurrentDarts = new ArrayList(currentDarts);
+		Collections.sort(tempCurrentDarts);
+		for(ArrayList<Integer> arrayList : this.exploredSolutions){
+			ArrayList<Integer> tempExploredSolution = new ArrayList(arrayList);
+			Collections.sort(tempExploredSolution);
+			if (tempCurrentDarts.equals(tempExploredSolution)){
+				return true;
+			}
+		}
 		return false;
 	}
 	
-	private int sumDarts(ArrayList<Integer> darts){
+	private int sumDarts(ArrayList<Integer> darts) {
 		int sum = 0;
 		for(Integer dart : darts) {
 			sum += dart;
