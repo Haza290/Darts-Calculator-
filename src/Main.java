48adaa691b4c467643adc42edcb73a29e0f37325
hschneider <harry.schneider@aquilaheywood.co.uk>
2018-09-05 14:13:38 +0100

    Updated explored function and changed last dart to be a double or bullseye

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Main
 {
 	private int target = 60;
-	private int maxNumOfDarts = 6;
+	private int maxNumOfDarts = 3;
 	private ArrayList<ArrayList<Integer>> solutions;
 	private ArrayList<ArrayList<Integer>> exploredSolutions;
 	
-	private int[] allDartScores = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,
-		19,20,21,22,24,25,26,27,28,30,32,33,34,36,38,39,40,42,44,45,46,48,50,51,
-		52,54,56,57,60};
-	private int[] endingDartScores = {2,3,4,6,8,9,10,12,14,15,16,18,20,21,22,24,
-		25,26,27,28,30,32,33,34,36,38,39,40,42,44,45,46,48,50,51,52,54,56,57,60};
+	private int[] allDartScores = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,
+		19,20,21,22,24,25,26,27,28,30,32,33,34,36,38,39,40,42,44,45,46,48,50,51,52,
+		54,56,57,60};
+	private int[] endingDartScores = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,
+			34,36,38,40,42,44,46,48,50,52,54,56,60};
 	
 	public static void main(String[] args) {
 		new Main();
@@ -28,10 +28,10 @@ public class Main
 	
 	
 	private void loopback(final ArrayList<Integer> currentDarts) {
-		//if (isAlreadyExplored(currentDarts)) {
-		//	return;
-		//}
-		//this.exploredSolutions.add(currentDarts);
+		if (isAlreadyExplored(currentDarts)) {
+			return;
+		}
+		this.exploredSolutions.add(Collections.sort(new ArrayList(currentDarts)));
 		int sumOfCurrentDarts = sumDarts(currentDarts);
 		if (target == sumOfCurrentDarts) {
 			solutions.add(currentDarts);
@@ -80,9 +80,7 @@ public class Main
 		ArrayList<Integer> tempCurrentDarts = new ArrayList(currentDarts);
 		Collections.sort(tempCurrentDarts);
 		for(ArrayList<Integer> arrayList : this.exploredSolutions){
-			ArrayList<Integer> tempExploredSolution = new ArrayList(arrayList);
-			Collections.sort(tempExploredSolution);
-			if (tempCurrentDarts.equals(tempExploredSolution)){
+			if (tempCurrentDarts.equals(arrayList)){
 				return true;
 			}
 		}
